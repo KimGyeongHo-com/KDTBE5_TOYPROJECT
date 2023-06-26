d1c852cda0c160b8653867da6250193ea2ae8d8d
KH <kkh96071@gmail.com>
2023-06-26 23:53:04 +0900

    [feat] TeamService 구현
    TeamDao의 기능을 수행하는 TeamService의 getAllTeam().

--- src/main/java/toy/example/KDTBE5_TOYPROJECT/service/TeamService.java
+++ src/main/java/toy/example/KDTBE5_TOYPROJECT/service/TeamService.java
@@ -1,4 +1,17 @@
 package toy.example.KDTBE5_TOYPROJECT.service;
 
+import lombok.RequiredArgsConstructor;
+import toy.example.KDTBE5_TOYPROJECT.dao.TeamDao;
+import toy.example.KDTBE5_TOYPROJECT.model.Team;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public class TeamService {
+
+    private final TeamDao teamDao;
+
+    public List<Team> getAllTeam() {
+        return teamDao.getAllTeam();
+    }
 }
