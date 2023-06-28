57df872c71108cbe9e2db0341cb170dc4a050ee5
GyuChangSeong <ahdzlq12@gmail.com>
2023-06-29 04:15:38 +0900

    [feat]:OutPlayerRespDTO 클래스 추가

---
+++ src/main/java/toy/example/KDTBE5_TOYPROJECT/dto/outplayer/OutPlayerRespDTO.java
@@ -0,0 +1,17 @@
+package toy.example.KDTBE5_TOYPROJECT.dto.outplayer;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+
+@Getter
+@Builder
+public class OutPlayerRespDTO {
+    private int id;
+    private String name;
+    private String position;
+    private String reason;
+    private Timestamp createdAt;
+}
