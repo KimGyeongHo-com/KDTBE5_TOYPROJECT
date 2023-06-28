6ca9cb76b005ad5cb5dfebe487c0916c537cb775
GyuChangSeong <ahdzlq12@gmail.com>
2023-06-29 03:56:57 +0900

    [feat]:전체야구장목록보기
    Add method
    1.Stadium서비스: getStadiumList
    2.StadiumDao: getStadiumList - jdbc 쿼리

--- src/main/java/toy/example/KDTBE5_TOYPROJECT/dao/StadiumDao.java
+++ src/main/java/toy/example/KDTBE5_TOYPROJECT/dao/StadiumDao.java
@@ -22,4 +22,26 @@ public class StadiumDao {
     }
 
     //메소드들 작성하시면 됩니다.
+    public List<Stadium> getStadiumList() {
+        List<Stadium> stadiumList = new ArrayList<>();
+        String query = "SELECT * FROM stadium";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Stadium stadium = Stadium.builder()
+                        .id(resultSet.getInt("id")) //colum명을 이렇게 직접 넣나??
+                        .name(resultSet.getString("name"))
+                        .createDate(resultSet.getTimestamp("created_at"))
+                        .build();
+
+                stadiumList.add(stadium);
+            }
+        }catch(SQLException e){
+            Logger.getLogger("경기장 select: " + e.getMessage());
+        }
+        return stadiumList;
+    }
+
 }
--- src/main/java/toy/example/KDTBE5_TOYPROJECT/service/StadiumService.java
+++ src/main/java/toy/example/KDTBE5_TOYPROJECT/service/StadiumService.java
@@ -1,4 +1,14 @@
 package toy.example.KDTBE5_TOYPROJECT.service;
 
 public class StadiumService {
+
+    private StadiumDao stadiumDao;
+
+    public StadiumService(StadiumDao stadiumDao){
+        this.stadiumDao = stadiumDao;
+    }
+
+    public List<Stadium> getStadiumList() {
+        return stadiumDao.getStadiumList();
+    }
 }
