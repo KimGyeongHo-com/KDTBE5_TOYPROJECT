2185f0d75b5a7b8a238b801f4e83ac96e4ddabe7
KH <kkh96071@gmail.com>
2023-06-29 13:03:54 +0900

    [fix] TeamService 수정
    의존성 추가

--- src/main/java/toy/example/KDTBE5_TOYPROJECT/service/TeamService.java
+++ src/main/java/toy/example/KDTBE5_TOYPROJECT/service/TeamService.java
@@ -2,8 +2,12 @@ package toy.example.KDTBE5_TOYPROJECT.service;
 
 import lombok.RequiredArgsConstructor;
 import toy.example.KDTBE5_TOYPROJECT.dao.TeamDao;
+import toy.example.KDTBE5_TOYPROJECT.dto.PositionRespDto;
+import toy.example.KDTBE5_TOYPROJECT.dto.TeamRespDTO;
+import toy.example.KDTBE5_TOYPROJECT.model.Player;
 import toy.example.KDTBE5_TOYPROJECT.model.Team;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -11,7 +15,10 @@ public class TeamService {
 
     private final TeamDao teamDao;
 
-    public List<Team> getAllTeam() {
+
+    public List<TeamRespDTO> getAllTeam(TeamDao teamDao) {
         return teamDao.getAllTeam();
     }
+
+
 }
