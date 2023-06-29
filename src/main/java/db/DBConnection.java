4394479b26b2f2e3cb1529642ceec6c17a406208
KH <kkh96071@gmail.com>
2023-06-29 12:58:28 +0900

    [fix] DBConnection
    DB커넥션 싱글톤으로 수정

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -5,7 +5,11 @@ import java.sql.DriverManager;
 
 
 public class DBConnection {
+    private static Connection connection;
     public static Connection getInstance(){
+        if (connection != null){
+            return connection;
+        }
         // MySQL 연결 정보
         String url = "jdbc:mysql://localhost:3306/metadb";
         String username = "root";
@@ -16,7 +20,7 @@ public class DBConnection {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, username,
                     password);
-            System.out.println("debug : DB has been connected");
+//            System.out.println("debug : DB has been connected");
             return connection;
         } catch (Exception e) {
             e.printStackTrace();
